e747a7928c33feb704fb0fa02a3fca53aa33dbeb
DimonnPokemonn <dimon170280@gmail.com>
2023-01-30 23:03:11 -0500

    The Handler Class created

---
+++ src/Handler.java
@@ -0,0 +1,67 @@
+import javax.swing.*;
+import java.awt.*;
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// !!! V - VIEW This entire file is about VIEW.
+// !!! CREATED BY DMYTRO
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+public class Handler {
+
+  /////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // !!! V-VIEW PART - START
+  // !!! CREATED BY DMYTRO
+
+  private static JPanel formatPanel;
+  private static GameBoard gamePanel;
+  private static ScoreBoard scoreBoard;
+
+
+  private static final int FRAME_WIDTH  = 725;
+  private static final int FRAME_HEIGHT = 750;
+  
+  public static void main(String[] args) {
+    loadGame();
+  }
+  
+  /**
+   * Runs the game. There is currently no point in having a game loop, since the only
+   * time the game needs to be re-painted is when the user moves a card.
+   */
+
+  public static void reloadGame(int mode) {
+    formatPanel.remove(gamePanel);
+    formatPanel.remove(scoreBoard);
+    gamePanel = new GameBoard(mode);
+    scoreBoard = gamePanel.getScoreBoard();
+    formatPanel.add(gamePanel);
+    formatPanel.add(scoreBoard, BorderLayout.SOUTH);
+    formatPanel.revalidate();
+  }
+  
+  /**
+   * Initializes game objects
+   */
+  public static void loadGame() {
+    JFrame gameFrame = new JFrame("Solitaire Game");
+
+    formatPanel = new JPanel();
+    gamePanel = new GameBoard();
+    scoreBoard = gamePanel.getScoreBoard();
+    Menu gameMenu = new Menu();
+
+    gameFrame.setJMenuBar(gameMenu.createMenu());
+    gameFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    gameFrame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
+
+    formatPanel.setLayout(new BorderLayout());
+    formatPanel.add(gamePanel);
+    formatPanel.add(scoreBoard, BorderLayout.SOUTH);
+    gameFrame.add(formatPanel);
+    gameFrame.setVisible(true);
+    gameFrame.setResizable(false);
+  }
+}
+
+// !!! V-VIEW PART - FINISH
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
